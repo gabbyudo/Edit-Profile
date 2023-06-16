6e40f8ee134b5d27d711c93414cf14d6c8223089
Udo Gabriel <gabbyy305400@gmail.com>
2023-06-16 22:19:30 +0100

    Setting progress bar

--- app/src/main/java/com/example/editprofile/MainActivity.kt
+++ app/src/main/java/com/example/editprofile/MainActivity.kt
@@ -6,10 +6,15 @@ import android.os.Bundle
 import android.util.Log
 import android.util.Patterns
 import android.view.View
+import android.widget.Toast
 import androidx.core.widget.doAfterTextChanged
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.editprofile.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
@@ -21,35 +26,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        binding.progressBar.visibility = View.VISIBLE
+        binding.username.doAfterTextChanged {
+            binding.button.isEnabled =
+                Patterns.EMAIL_ADDRESS.matcher(binding.username.text.toString()).matches()
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(binding.username.text.toString()).matches())
+            {  binding.username.error = "not correct"}
+        }
         viewModel.name.observe(this, Observer {
             binding.username.setText(it)
-           binding.username.doAfterTextChanged {
-               binding.button.isEnabled =
-                   Patterns.EMAIL_ADDRESS.matcher(binding.username.text.toString()).matches()
-           }
 
-           /* binding.username.setOnFocusChangeListener{_, focused->
-                if (!focused){
-                    binding.usernameLayout.helperText =validPattern()
-                }
-            }*/
-            /*binding.button.setOnClickListener{
-                it.isSelected != it.isSelected
+            binding.progressBar.visibility = View.GONE
 
-            }*/
         })
         binding.switch1.setOnCheckedChangeListener{ buttonView, isChecked ->
            // it.isEnabled = isChecked
         }
-        //binding.progressBar.visibility = View.VISIBLE
         viewModel.getName()
     }
-    /*fun validPattern():String? {
-        val userName = binding.username.text.toString()
-        if (!Patterns.EMAIL_ADDRESS.matcher(userName).matches()){
-            return "not correct"
-        }
-        return null
-    }*/
+
 
 }
--- app/src/main/java/com/example/editprofile/MainViewModel.kt
+++ app/src/main/java/com/example/editprofile/MainViewModel.kt
@@ -3,16 +3,25 @@ package com.example.editprofile
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainViewModel: ViewModel() {
     private val _name = MutableLiveData<String>()
     val name: LiveData<String>
         get() = _name
 
-    fun getName() : String{
-        val name = "dynamics"
-        _name.value= name
-        return _name.value.toString()
+    fun getName() {
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(10000)
+            val name = "dynamics"
+            _name.value= name
+        }
+
+
         }
 
 
